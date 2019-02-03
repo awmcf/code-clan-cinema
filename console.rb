47002dd8f29daa5f3337b700a0c28fddd25e72e6
awmcf <aaronmcfaull@gmail.com>
2019-02-03 15:03:51 +0000

    Models have Update functions

--- console.rb
+++ console.rb
@@ -20,5 +20,14 @@ film_2.save()
 ticket_1 = Ticket.new({'customer_id' => '24', 'film_id' => '22'})
 ticket_1.save()
 
+customer_1.name = "Aaronovitch"
+customer_1.update()
+
+film_2.title = "A great film about dogs"
+film_2.update()
+
+ticket_1.customer_id = 24
+ticket_1.update()
+
 binding.pry
 nil
